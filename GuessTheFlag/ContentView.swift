661416cb0a64e2befd8baa23916715f0b39745f0
thaddeus0605 <tdronski@gmail.com>
2022-12-08 21:22:40 -0600

    Added functionality to show players current score, display alerts stating the correct answer, and reset the game once 8 questions have been released.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,6 +15,14 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var playerScore = 0
+    
+    @State private var alertMessage = ""
+    
+    @State private var gameCounter = 0
+    
+    @State private var gameOverAlert = false
+    @State private var gameOverText = "Game Over!"
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -29,7 +37,7 @@ struct ContentView: View {
                     .foregroundColor(.white)
                 Spacer()
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(playerScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 Spacer()
@@ -65,24 +73,49 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text(alertMessage)
+        }
+        .alert(gameOverText, isPresented: $gameOverAlert) {
+            Button("Reset Game", action: reset)
+        } message: {
+            Text("Game over!")
         }
     }
     
     func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreTitle = "Correct"
-          } else {
-            scoreTitle = "Wrong"
-        }
+        gameCounter += 1
         
-        showingScore = true
+        if gameCounter < 9 {
+            if number == correctAnswer  {
+                scoreTitle = "Correct"
+                playerScore += 1
+                alertMessage = "You got it right!"
+                showingScore = true
+              } else {
+                scoreTitle = "Wrong"
+                alertMessage = "Yeah sorry bud, Thats the flag of \(countries[number])"
+                showingScore = true
+            }
+        } else {
+            showingScore = false
+            gameOverAlert = true
+            print("Game Over")
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    func reset() {
+        askQuestion()
+        gameOverAlert = false
+        gameCounter = 0
+        playerScore = 0
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
